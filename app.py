f513ebea06a8d1cea26b11a330926b157f8eacdb
Laura Anna Laizāne <laura.anna.laizane@gmail.com>
2026-04-28 20:35:27 +0300

    piektais upload

--- app.py
+++ app.py
@@ -23,9 +23,10 @@ def total_show(gender_id):
     conn = get_db_connection()
     Total_score = conn.execute(
         """
-        SELECT Total_score.*, Country.Flag AS flag 
+        SELECT Total_score.*, Country.Flag AS flag, Gender.Gender AS gender
         FROM Total_score
-        LEFT JOIN Country ON Total_score.Country_id = Country.id
+        LEFT JOIN Country ON Total_score.Country_id = Country.id 
+        LEFT JOIN Gender ON Total_score.Gender_id = Gender.id
         WHERE Total_score.Gender_id = ?
         """,
         (gender_id,),
@@ -41,6 +42,21 @@ def relay():
     conn.close()
     return render_template("relay.html", Gender=Gender)
 
+@app.route("/stafete/<int:gender_id>")
+def relay_show(gender_id):
+    conn = get_db_connection()
+    Relay = conn.execute(
+        """
+        SELECT Nations.*, Country.Flag AS flag, Country.Country as country, Gender.Gender AS gender
+        FROM Nations
+        LEFT JOIN Country ON Nations.Country_id = Country.id
+        LEFT JOIN Gender ON Nations.Gender_id = Gender.id
+        WHERE Nations.Gender_id = ? AND Nations.Discipline_id = 2
+        """,
+        (gender_id,),
+    ).fetchall()
+    conn.close()
+    return render_template("relay_show.html", Relay=Relay)
 
 @app.route("/valstu")
 def nation():
@@ -48,6 +64,22 @@ def nation():
     Gender = conn.execute("SELECT * FROM Gender LIMIT 2").fetchall()
     conn.close()
     return render_template("nation.html", Gender=Gender)
+
+@app.route("/valstu/<int:gender_id>")
+def nation_show(gender_id):
+    conn = get_db_connection()
+    Cup = conn.execute(
+        """
+        SELECT Nations.*, Country.Flag AS flag, Country.Country as country, Gender.Gender AS gender
+        FROM Nations
+        LEFT JOIN Country ON Nations.Country_id = Country.id
+        LEFT JOIN Gender ON Nations.Gender_id = Gender.id
+        WHERE Nations.Gender_id = ? AND Nations.Discipline_id = 3
+        """,
+        (gender_id,),
+    ).fetchall()
+    conn.close()
+    return render_template("nation_show.html", Cup=Cup)
 
 
 def get_db_connection():
